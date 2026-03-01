6ce14227303cad9be9daada78c363103db08850d
Mansiiikk <140382919+MansiKarki@users.noreply.github.com>
2026-03-01 14:46:24 +0530

    Added ForgotPassword and reset Password

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, signup, getMe } from "../controllers/authController.js";
+import { login, signup, getMe, forgotPassword, resetPassword } from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/login", login);
 router.post("/signup", signup);
 router.get("/me", protect, getMe);
-
+router.post("/forgot-password", forgotPassword);
+router.put("/reset-password/:resetToken", resetPassword);
 
 export default router;
